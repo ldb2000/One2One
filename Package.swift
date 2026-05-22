642d0eeeb52e1306b23e1ff8cb064e4a3f1d2c82
DE BERTI Laurent <laurent.deberti@gmail.com>
2026-05-22 08:28:14 +0200

    build(markdown): add swift-markdown dependency

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         .package(url: "https://github.com/Blaizzy/mlx-audio-swift.git", branch: "main"),
         .package(url: "https://github.com/ml-explore/mlx-swift.git", from: "0.30.6"),
         .package(url: "https://github.com/soniqo/speech-swift", branch: "main"),
+        .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.6.0"),
     ],
     targets: [
         .executableTarget(
@@ -22,6 +23,7 @@ let package = Package(
                 .product(name: "MLXAudioCore", package: "mlx-audio-swift"),
                 .product(name: "MLX", package: "mlx-swift"),
                 .product(name: "SpeechVAD", package: "speech-swift"),
+                .product(name: "Markdown", package: "swift-markdown"),
             ],
             path: "OneToOne",
             resources: [
